bcfafdaa1ce775f456a1760d590c63554d4b4dc9
VictorCrespo <VictorCrespo.Developer@gmail.com>
2021-09-30 16:58:02 -0700

    Revert "Cambio consulta metodo ObtenerArticulosDeSubcategoria"
    This reverts commit 71113bc67d255a4dded1555013c13f11433a84de.

--- WS/wsComunicacion.asmx.cs
+++ WS/wsComunicacion.asmx.cs
@@ -3429,9 +3429,9 @@ WHERE
 
         }
         [WebMethod(Description = "Regresa Nombre y codigo de los articulos pertenecientes a esa categoria")]
-        public string ObtenerArticulosDeSubcategoria(string SubcategoriasID)
+        public string ObtenerArticulosDeSubcategoria(string SubcategoriaID)
         {
-            String sQry = "select ArticulosID,Codigo,Nombre from Articulos where AppSubCategoriasID IN("+SubcategoriasID+") order by Nombre asc";
+            String sQry = "select ArticulosID,Codigo,Nombre from Articulos where AppSubCategoriasID = "+SubcategoriaID+" order by Nombre asc";
             System.Data.DataSet ds;
             System.Xml.XmlElement xmlElement;
             try
