921eb773e6ce278834900cdabdc4a7fe228bf76e
Paco Macumba <79169985+EnriqueLey@users.noreply.github.com>
2022-05-05 10:49:28 -0700

    Se quito el parametro al metodo ObtenerEmpleadosSION

--- WS/wsComunicacion.asmx.cs
+++ WS/wsComunicacion.asmx.cs
@@ -4309,7 +4309,7 @@ WHERE
 
      //----------------[Obtener Empleados SION]------------------
     [WebMethod(Description = "Obtener Empleados SION")]
-        public string ObtenerEmpleadosSION(String sOrdenCompra)
+        public string ObtenerEmpleadosSION()
         {
             String sQry = @"select 
 	                            Usuarios.Nombre as Usuarios,	
